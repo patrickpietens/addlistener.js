a253be932c5ff37fc1001bb264e2beb24d2bf515
Patrick Pietens <hello@patrickpietens.com>
2016-02-07 20:08:55 +0100

    Added function documentation

--- src/index.js
+++ src/index.js
@@ -1,40 +1,49 @@
 'use strict';
 
 /**
- * Adds addEventListener function to NodeList objects
- * @param  {[type]}   eventName String representing the event name to listen to
- * @param  {function} handler   Function which is executed if the event is triggered
+ * Adds `addEventListener` to `NodeList` objects. This methods adds event handlers to all
+ * `Node` childs of the `NodeList` object.
+ * @param  {string}   eventName     String representing the name of the event
+ * @param  {function} handler       Handler which is invoked if the event is triggered
+ * @param  {boolean}  useCapture    Boolean that specifies whether the event should be executed in the capturing or in the bubbling phase
  */
-NodeList.prototype.addEventListener = function (eventName, handler) {
-    [].forEach.call(this, (element) => {
-        element.addEventListener(eventName, handler);
-    });
+if (!NodeList.prototype.addEventListener) {
+    NodeList.prototype.addEventListener = function (eventName, handler, useCapture = false) {
+        [].slice.call(this).forEach((element) => {
+            element.addEventListener(eventName, handler, useCapture);
+        });
+    };
+}
+
+/**
+ * Adds `removeEventListener` to `NodeList` objects. This methods removes event handlers to all
+ * `Node` childs of the `NodeList` object.
+ * @param  {string}   eventName     String representing the name of the event
+ * @param  {function} handler       Handler which is invoked if the event is triggered
+ */
+ if (!NodeList.prototype.removeEventListener) {
+    NodeList.prototype.removeEventListener = function (eventName, handler) {
+        [].slice.call(this).forEach((element) => {
+            element.removeEventListener(eventName, handler);
+        });
+    };
+}
+
+/**
+ * Adds a shortcut to the `Node`, `NodeList` and `window` object for the `addEventListener` function
+ * @param  {string}   eventName     String representing the name of the event
+ * @param  {function} handler       Handler which is invoked if the event is triggered
+ * @param  {boolean}  useCapture    Boolean that specifies whether the event should be executed in the capturing or in the bubbling phase
+ */
+NodeList.prototype.on = Node.prototype.on = window.on = function (eventName, handler, useCapture = false) {
+    this.addEventListener(eventName, handler, useCapture);
 };
 
 /**
- * Adds removeEventListener function to NodeList objects
- * @param  {[type]}   eventName String representing the event name to listen to
- * @param  {function} handler   Function which is executed if the event is triggered
- */
-NodeList.prototype.addEventListener = function (eventName, handler) {
-	[].forEach.call(this, (element) => {
-        element.removeEventListener(eventName, handler);
-    });
-};
-
-/**
- * Adds shortcuts for the addEventListener methods
- * @param  {[type]}   eventName String representing the event name to listen to
- * @param  {function} handler   Function which is executed if the event is triggered
- */
-NodeList.prototype.on = Node.prototype.on = window.on = function (eventName, handler) {
-    this.addEventListener(eventName, handler);
-};
-
-/**
- * Adds shortcuts for the removeEventListener methods
- * @param  {[type]}   eventName String representing the event name to listen to
- * @param  {function} handler   Function which is executed if the event is triggered
+ * Adds a shortcut to the `Node`, `NodeList` and `window` object for the `removeEventListener` function
+ * @param  {string}   eventName     String representing the name of the event
+ * @param  {function} handler       Handler which is invoked if the event is triggered
+ * @param  {boolean}  useCapture    Boolean that specifies whether the event should be executed in the capturing or in the bubbling phase
  */
 NodeList.prototype.off = Node.prototype.off = window.off = function (eventName, handler) {
     this.removeEventListener(eventName, handler);
